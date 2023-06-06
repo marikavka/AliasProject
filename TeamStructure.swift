69edab6e07f6fd7de6b8f91a356c5e0b0a3c3e34
Marikavka <mkupchinskaia@gmail.com>
2023-06-06 20:41:02 +0300

    now we can choose teams

---
+++ TeamStructure.swift
@@ -0,0 +1,14 @@
+//
+//  TeamStructure.swift
+//  AliasProject
+//
+//  Created by Мария Купчинская on 06.06.2023.
+//
+
+import Foundation
+
+struct Team {
+    var imageName = ""
+    var name = ""
+    var points = 0
+}
