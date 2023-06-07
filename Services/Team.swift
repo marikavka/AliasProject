b6dc47c0b38b098d894a71b47e52c1c1cafaad00
Marikavka <mkupchinskaia@gmail.com>
2023-06-07 11:29:42 +0300

    Team struct

---
+++ Services/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  AliasProject
+//
+//  Created by Мария Купчинская on 07.06.2023.
+//
+
+import Foundation
+
+struct Team {
+    var imageName = ""
+    var name = ""
+    var points = 0
+}
